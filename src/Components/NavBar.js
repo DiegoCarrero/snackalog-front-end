26bc5d00ea4002c9912f7326dc294aca2ab71aea
Diegocarrero <diegocar2413@gmail.com>
2023-05-09 19:33:03 -0400

    edit navbar links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,8 +4,11 @@ export default function NavBar() {
   return (
     <div className='NavBar'>
       <h1>
-        <Link to='/snacks'>Snackalog</Link>
+        <Link to='/'>Snackalog</Link>
       </h1>
+      <h2>
+        <Link to='/snacks'>All Snacks</Link>
+      </h2>
       <button>
         <Link to='/snacks/new'>Add a Snack</Link>
       </button>
