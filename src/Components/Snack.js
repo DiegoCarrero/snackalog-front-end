3d27f80a0859fc4fd43fe191374420754eb6cfd8
Diegocarrero <diegocar2413@gmail.com>
2023-05-09 19:08:32 -0400

    add snack data to show page, state, hooks and get request

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function Snack() {
+
+  const [snack, setSnack] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${API}/snacks/${id}`)
+      .then((response) => setSnack(response.data))
+      .catch((e) => console.warn('catch', e)) 
+  }, [id, API])
+
   return (
     <div>
-      
+
+      {snack.is_healthy ? (
+        <span>(is healthy icon)</span>
+      ) : null }
+      {snack.name}
+      {snack.image}
+
+      <button>
+        <Link to={`/snacks/${id}/edit`} >Edit Snack</Link>
+      </button>
+
+      <button onClick={handleDelete}>Delete Snack</button>
+
     </div>
   )
 }
