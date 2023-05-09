1d12dfd60a848131c928d440e8d99bec3b05204d
arerimr <raydelys.morrobelr@gmail.com>
2023-05-09 18:42:50 -0400

    intall axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home.js'
 import './App.css';
+import Snacks from './Components/Snacks';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/snacks' element={<Snacks />} />
+        <Route path='/snacks/:id' element={<Home />} />
+      </Routes>
     </div>
   );
 }
