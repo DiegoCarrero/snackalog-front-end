b48a6e1b1941abd4376a83f16ad519dbc5435b7e
Diegocarrero <diegocar2413@gmail.com>
2023-05-09 19:54:42 -0400

    add state, put request and handletextchange to EditSnack

--- src/Components/EditSnack.js
+++ src/Components/EditSnack.js
@@ -1,9 +1,45 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
+const API = process.env.REACT_APP_API_URL;
 
 export default function EditSnack() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    let { id } = useParams();
+    let navigate = useNavigate();
+
+    const [snack, setSnack] = useState(
+        {
+            name: '',
+            image: '',
+            calories: '',
+            fiber: '',
+            sodium: '',
+            sugar: '',
+            gluten_free: false,
+            flavor_profile: '',
+            is_healthy: '',
+        }
+    );
+
+    const updateSnack = (snackToUpdate) => {
+        axios.put(`${API}/snacks/${id}`, snackToUpdate)
+            .then(() => {
+                navigate(`/snacks/${id}`)
+            },
+                (error) => console.error(error)
+            )
+            .catch((c) => console.warn("catch", c))
+    }
+
+    const handleTextChange = (event) => {
+        setSnack({ ...snack, [event.target.id]: event.target.value });
+    };
+
+    return (
+        <div>
+
+        </div>
+    )
 }
