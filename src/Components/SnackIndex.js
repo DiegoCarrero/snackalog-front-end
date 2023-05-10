9934e11b761787d7fef61cd83e965cfb7d6fe06f
arerimr <raydelys.morrobelr@gmail.com>
2023-05-09 20:22:13 -0400

    added filter properties

--- src/Components/SnackIndex.js
+++ src/Components/SnackIndex.js
@@ -19,9 +19,9 @@ export default function SnackIndex({ snack }) {
             <Link to={`/snacks/${snack.id}`}><img src={snack.image} alt={snack.name} /></Link>
             <span><strong>{snack.flavor_profile}</strong></span>
             <span><strong>Calories: </strong>{snack.calories}</span>
-            <span><strong>Fiber: </strong>{snack.fiber}</span>
-            <span><strong>Sodium: </strong>{snack.sodium}</span>
-            <span><strong>Sugar: </strong>{snack.sugar}</span>
+            <span><strong>Fiber: </strong>{snack.fiber}g</span>
+            <span><strong>Sodium: </strong>{snack.sodium}mg</span>
+            <span><strong>Sugar: </strong>{snack.sugar}g</span>
             <span><strong>{snack.gluten_free ? "Gluten Free" : null}</strong></span>
         </div>
     )
--- src/Components/Snacks.js
+++ src/Components/Snacks.js
@@ -5,22 +5,72 @@ import SnackIndex from './SnackIndex';
 const API = process.env.REACT_APP_API_URL;
 
 export default function Snacks() {
-    const [snacks, setSnack] = useState([]);
+    const [snacks, setSnacks] = useState([]);
 
     useEffect(() => {
         axios.get(`${API}/snacks`)
-            .then((res) => setSnack(res.data))
+            .then((res) => setSnacks(res.data))
             .catch((e) => console.warn(e))
     }, [])
 
     return (
         <div className='snacks'>
-            {
-                snacks.map((snack) => {
-                    return <SnackIndex key={snack.id} snack={snack} />
-                })
-            }
-            <button>New Snack</button>
+            <h1>SNACKS</h1>
+            <form>
+                <span>
+                    <input
+                        id="is_healthy"
+                        type="checkbox"
+                        onChange={true}
+                        checked={false}
+                    />
+                    <label htmlFor="healthy">Healthy Snacks</label>
+                </span>
+                <span>
+                    <input
+                        id="low_sodium"
+                        type="checkbox"
+                        onChange={true}
+                        checked={false}
+                    />
+                    <label htmlFor="lowSodium">Low Sodium</label>
+                </span>
+                <span>
+                    <input
+                        id="low_calorie"
+                        type="checkbox"
+                        onChange={true}
+                        checked={false}
+                    />
+                    <label htmlFor="lowCalorie">Low Calorie</label>
+                </span>
+                <span>
+                    <input
+                        id="low_sugar"
+                        type="checkbox"
+                        onChange={true}
+                        checked={false}
+                    />
+                    <label htmlFor="lowSugar">Low Sugar</label>
+                </span>
+                <span>
+                    <input
+                        id="high_fiber"
+                        type="checkbox"
+                        onChange={true}
+                        checked={false}
+                    />
+                    <label htmlFor="highFiber">High Fiber</label>
+                </span>
+            </form>
+            <section>
+                {
+                    snacks.map((snack) => {
+                        return <SnackIndex key={snack.id} snack={snack} />
+                    })
+                }
+                <button>New Snack</button>
+            </section>
         </div>
     )
 }
