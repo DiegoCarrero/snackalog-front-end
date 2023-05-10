4978b3d6096d314ad27a90bd58667d61c884ca66
arerimr <raydelys.morrobelr@gmail.com>
2023-05-10 19:28:26 -0400

    about us finished

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,6 @@
-
+import { ExternalLink } from "react-external-link"
+import diego from '../assets/diego.jpeg'
+import rmr from '../assets/rmr.jpeg'
 
 export default function Home() {
   return (
@@ -7,7 +9,34 @@ export default function Home() {
         <p>Description of the app and how to use it</p>
       </section>
       <section className="AboutUs">
-        <h2 className="Subtitle">About Us</h2>
+        <h2 className="about">About Us</h2>
+        <div className="about">
+          <section className="team">
+            <div className="diego">
+              <img src={diego} alt="Diego Carrero" />
+            </div>
+            <div className="info">
+              <h4>Diego Carrero</h4>
+              <span>Full Stack Web Developer</span>
+              <span>Pursuit Fellow 9.4 N&W</span>
+              <p>Github: <ExternalLink href="https://github.com/DiegoCarrero" target="_blank">@DiegoCarrero</ExternalLink></p>
+              <p>Linkedin: <ExternalLink href="https://www.linkedin.com/in/diegocarrero/" target="_blank">Diego Carrero</ExternalLink></p>
+            </div>
+
+            <hr />
+
+            <div className="info">
+              <h4>Raydelys Morrobel Reyes</h4>
+              <span>Full Stack Web Developer</span>
+              <span>Pursuit Fellow 9.4 N&W</span>
+              <p>Github: <ExternalLink href="https://github.com/arerimr" target="_blank">@arerimr</ExternalLink ></p>
+              <p>Linkedin: <ExternalLink href="https://www.linkedin.com/in/raydelysmr" target="_blank">Raydelys M R</ExternalLink></p>
+            </div>
+            <div className="raydelys">
+              <img src={rmr} alt="Raydelys MR" />
+            </div>
+          </section>
+        </div>
       </section>
     </div>
   )
