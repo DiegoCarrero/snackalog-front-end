c43c3e49d1588ca88d4e0bcbc9013497504f878a
arerimr <raydelys.morrobelr@gmail.com>
2023-05-10 20:33:59 -0400

    add new form and css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Snacks from './Components/Snacks';
 import NotFound from './Components/NotFound.js';
 import NavBar from './Components/NavBar.js';
 import Snack from './Components/Snack.js';
+import NewSnack from './Components/NewSnack.js';
+import EditSnack from './Components/EditSnack.js';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path='/snacks' element={<Snacks />} />
         <Route path='/snacks/:id' element={<Snack />} />
         <Route path='*' element={<NotFound />} />
+        <Route path='/snacks/new' element={<NewSnack />} />
+        <Route path='/snacks/:id/edit' element={<EditSnack />} />
       </Routes>
     </div>
   );
